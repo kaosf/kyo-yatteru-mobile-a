f9c29393e3ff3a80e360f6a49d68cd361dad1b3d
ka <ka.kaosf@gmail.com>
2013-11-02 23:00:20 +0900

    implement web view

--- src/org/kyo/yatteru/MainActivity.java
+++ src/org/kyo/yatteru/MainActivity.java
@@ -2,13 +2,28 @@ package org.kyo.yatteru;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.webkit.WebView;
+import android.view.KeyEvent;
 
 public class MainActivity extends Activity {
+
+  private WebView wv;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    wv = (WebView) findViewById(R.id.webview);
+    wv.getSettings().setJavaScriptEnabled(true);
+    wv.loadUrl(getString(R.string.uri));
+  }
+
+  public boolean onKeyDown(int keyCode, KeyEvent event) {
+    if ((keyCode == KeyEvent.KEYCODE_BACK) && wv.canGoBack()) {
+      wv.goBack();
+      return true;
+    }
+    return super.onKeyDown(keyCode, event);
   }
 
 }
